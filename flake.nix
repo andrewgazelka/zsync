98c2775a5e2dea472ddc040ec9667e777b883c7f
Andrew Gazelka <andrew.gazelka@gmail.com>
2025-12-15 19:36:58 -0800

    fix: revert to pkgsCross for working cross-compilation

--- flake.nix
+++ flake.nix
@@ -61,32 +61,37 @@
 
     # Cross-compile zsync-agent for Linux (static musl) from any host
     crossAgentFor = hostSystem: targetArch: let
-      crossSystem =
+      hostPkgs = pkgsFor hostSystem;
+
+      # Use musl cross toolchain
+      crossPkgs =
+        if targetArch == "x86_64"
+        then hostPkgs.pkgsCross.musl64
+        else hostPkgs.pkgsCross.aarch64-multiplatform-musl;
+
+      cargoTarget =
         if targetArch == "x86_64"
         then "x86_64-unknown-linux-musl"
         else "aarch64-unknown-linux-musl";
 
-      # Import nixpkgs with cross-compilation support and rust-overlay
-      # This properly splices packages so the function form works
-      pkgs = import nixpkgs {
-        localSystem = hostSystem;
-        inherit crossSystem;
-        overlays = [rust-overlay.overlays.default];
-      };
-
-      cargoTarget = crossSystem;
-
-      # Function form works because pkgs has rust-overlay applied
-      craneLib = (crane.mkLib pkgs).overrideToolchain (p:
-        p.rust-bin.stable.latest.default.override {
-          targets = [crossSystem];
-        }
-      );
+      # Use hostPkgs toolchain (has rust-overlay), crane with crossPkgs
+      toolchain = rustToolchainFor hostPkgs;
+      craneLib = (crane.mkLib crossPkgs).overrideToolchain toolchain;
 
       commonArgs = {
         src = craneLib.cleanCargoSource ./.;
         strictDeps = true;
         CARGO_BUILD_TARGET = cargoTarget;
+        CARGO_BUILD_RUSTFLAGS = "-C target-feature=+crt-static";
+
+        # Use the cross compiler
+        "CARGO_TARGET_${hostPkgs.lib.toUpper (builtins.replaceStrings ["-"] ["_"] cargoTarget)}_LINKER" =
+          "${crossPkgs.stdenv.cc}/bin/${crossPkgs.stdenv.cc.targetPrefix}cc";
+
+        HOST_CC = "${hostPkgs.stdenv.cc}/bin/cc";
+
+        depsBuildBuild = [hostPkgs.stdenv.cc];
+        nativeBuildInputs = [crossPkgs.stdenv.cc];
       };
 
       cargoArtifacts = craneLib.buildDepsOnly commonArgs;
